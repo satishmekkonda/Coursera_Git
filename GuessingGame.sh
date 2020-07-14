3c5e5e24ab520bb42a36aff3531a087020e7fd5c
Satish Mekkonda <satishmekkonda@gmail.com>
2020-07-14 22:02:41 +0530

    Update GuessingGame.sh

--- GuessingGame.sh
+++ GuessingGame.sh
@@ -1,22 +1,27 @@
-#!/usr/bin/env bash
-
-function guess(){
-    true_ans=$(ls -l |grep "^-"|wc -l)
-    while true;
-    do
-        echo "pleas enter your guess"
-        read  number
-        if [ $number -lt $true_ans ]
-        then
-            echo "your guess is Less then the true number"
-        elif [ $number -gt $true_ans ]
-        then
-            echo "your guess is Greater then the true number"
-        else
-            echo " congratulation,you are right!"
-        break;
-        fi
-    done
+#!/usr/bin/env  bash
+echo "Welcome to guessing game "
+echo "Guess the number of files are there in directory"
+read number 
+echo "You entered: $number"
+file_count=$( ls | wc -l)
+function  guess {
+while [[ $number -ne $file_count ]]
+do 
+     if  [[ $number -lt $file_count ]]
+     then 
+          echo "You have entered $number"
+          echo "Your guess is too less."
+          echo "Please enter  a number again:"
+         read number
+     elif  [[ $number -gt $file_count ]]
+     then   
+         echo "You have entered $number"
+         echo "Your guess is too more."
+         echo "Please enter a number again :"
+         read number
+     fi
+done 
+echo "Congrats!!!! Your answer is correct!!!"
+echo  "Your directory contains $number"
 }
-echo "guess the files number in the current directory!"
-guess
+guess number file_count
